5a2b894c80901a832ddc52c0846c97c2956f5419
Prahlad Yeri <prahladyeri@yahoo.com>
2016-06-10 16:24:11 +0530

    updates

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2016  Prahlad Yeri <prahladyeri@yahoo.com>
+// Copyright (C) 2016  Prahlad Yeri
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
