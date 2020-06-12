607ebac287a7901327185176aeeee73f86fa59d3
André <akreienbring@gmail.com>
2020-06-12 11:46:33 +0200

    minor changes

--- startUp.js
+++ startUp.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Visual Alchemist listening on port 3000!");
+  console.log("Navigate your browser to 'http://localhost:3000'");
 });
 
